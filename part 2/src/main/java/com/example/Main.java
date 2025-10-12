2d7f897b7bc738c36287d0d1ad50a3adad08628d
Thorn Scheepers <ST10466183@rcconnect.edu.za>
2025-10-12 11:56:23 +0200

    Add files via upload

---
+++ part 2/src/main/java/com/example/Main.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+                MainGUI gui = new MainGUI();
+                gui.setVisible(true);
+            });
+            
+
+    }
+    }
